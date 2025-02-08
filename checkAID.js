3b6959d7ddb279147c253be58c1948837670b6ee
pancakes-proxy <zac_posey_12361_225@staffteam.learnhelp.cc>
2025-02-08 11:25:41 -0500

    Update checkAID.js

--- checkAID.js
+++ checkAID.js
@@ -1,1 +1,8 @@
-w
+   function checkAID() {
+            var password = prompt("Please enter the AID:");
+            if (password === "234567e35728414wr2rr4edvr28dbuye72ye8de2y932d-2fe3322xsPMS") {
+                window.location.href = "https://lantern.learnhelp.cc/home.html"
+            } else {
+                alert("Incorrect password. Please try again.");
+            }
+        }
